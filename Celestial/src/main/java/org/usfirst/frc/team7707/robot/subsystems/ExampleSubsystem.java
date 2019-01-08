99b663bb043515a96be774191afa67a49ffe4a09
splashface256 <24807471+splashface256@users.noreply.github.com>
2019-01-08 16:56:41 +0800

    Update ExampleSubsystem.java

--- Celestial/src/main/java/org/usfirst/frc/team7707/robot/subsystems/ExampleSubsystem.java
+++ Celestial/src/main/java/org/usfirst/frc/team7707/robot/subsystems/ExampleSubsystem.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package org.usfirst.frc.team1014.robot.subsystems;
+package org.usfirst.frc.team7707.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
